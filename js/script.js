0620b99796fcb31468dae644042d1dc7a98a3119
ansarq1 <ansardalisay9@gmail.com>
2024-08-22 20:28:13 +0800

    dark mode

--- js/script.js
+++ js/script.js
@@ -15,3 +15,14 @@ document.addEventListener("DOMContentLoaded", function(){
         image.onload = hideLoader;
     }
 });
+
+function lightDark() {
+    let image = document.querySelector("#profile");
+    let header = document.querySelector("#header");
+    let name = document.querySelector("#name");
+    let btn = document.querySelector("#workTogetherBtn");
+
+    header.classList.color = "black";
+    name.style.color = "black";
+    document.body.classList.toggle("dark");
+}
